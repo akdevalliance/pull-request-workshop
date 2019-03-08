fa8e45162a392de3b1349a70fce96c7c11cb7d13
Jeff Levin <jeff@levinology.com>
2019-03-07 20:21:09 -0900

    wrote some go

---
+++ rogue_math.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func add(int a, b) int{
+	return a+b
+}
+
+func subtract(int a, b) int{
+	return b-a
+}
+
+func multiply(int a,b) int{
+	return a*b
+}
+
+func divide(int a,b) int{
+	return b/a
+}
+
+func square(int a) int{
+	return a*a
+}
+
+func main() {
+	fmt.Println("GOLANG RAWKS")
+}
